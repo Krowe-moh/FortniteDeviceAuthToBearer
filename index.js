e452e06ec4bf0fffbec46566bb672f5ec26526c4
Pape68 <110750255+pape68@users.noreply.github.com>
2023-09-12 18:38:33 +0200

    Update index.js
    m

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const axios = require("axios")
+const axios = require("axios");
 
-let accid = ''
-let device = ''
-let secret = ''
+const accountId = '';
+const deviceId = '';
+const secret = '';
 
-axios({
-            method: 'post',
-            url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
-            },
-            data: `grant_type=device_auth&account_id=${accid}&device_id=${device}&secret=${secret}`
-        }).then(function(response) {
-  console.log(response.data)
-  axios({
-            method: 'get',
-            url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${response.data.access_token}`,
-            },
-        }).then(function(responsee) {
-  console.log(responsee.data)
-  })
-})
+async function main() {
+
+    const response = await axios({
+        method: 'post',
+        url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=',
+        },
+        data: `grant_type=device_auth&account_id=${accountId}&device_id=${deviceId}&secret=${secret}`
+    }).catch(error => { 
+        throw 'Authentication Failed'
+    });
+
+    console.log(response.data);
+
+    const response1 = await axios({
+        method: 'get', 
+        url: 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/exchange',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${response.data.access_token}`
+        }
+    });
+
+    console.log(response1.data);
+};
+
+main().catch(error => console.error(error));
